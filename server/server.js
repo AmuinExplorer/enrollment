f495020b86f6087c8def3db9f1c4b5cdf2985b72
Hxpere-2.0 <108596550+AmuinExplorer@users.noreply.github.com>
2024-03-15 14:30:43 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const ejs = require('ejs');
 const path = require('path');
 
@@ -12,7 +12,7 @@ app.set('views', path.join(__dirname, '../views')); // Use path.join for cross-p
 
 // MONGO DB CONNECTION
 // Connection URI - Replace with your MongoDB connection URI
-const uri = process.env.MONGODB_URI; // Use environment variable or local MongoDB URI
+const uri = process.env.MONGODB_URI || 'mongodb+srv://amuinharniel:iOAFA6o9vAFL1Ddt@blog.vi6xnkk.mongodb.net/?retryWrites=true&w=majority&appName=Blog'; // Use environment variable or local MongoDB URI
 
 // Database Name
 const dbName = 'portal';
@@ -30,6 +30,10 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
   }
 })();
 
+// Connect to the database
+const db = client.db(dbName);
+const collection = db.collection('enrollment-portal');
+
 // MONGO DB CONNECTION
 
 // Define a route for the root URL '/'
@@ -40,21 +44,41 @@ app.get('/', (req, res) => {
   res.sendFile(htmlPath);
 });
 
+app.get('/responses', async (req, res) => {
+  // Find all documents in the collection
+  const cursor = collection.find();
+  // Convert the cursor to an array of documents
+  const data = await cursor.toArray();
+  // Display all data from the database
+  res.render('template', { data }); // Render the template with the received data
+})
+
+app.get('/delete', async (req, res) => {
+  const { id } = req.query; // Assuming you receive the document ID from the query parameter
+  console.log(id)
+  try {
+    const result = await db.collection('enrollment-portal').deleteOne({ _id: new ObjectId(id) });
+    if (result.deletedCount === 1) {
+      res.redirect('/responses'); // Redirect to the responses page after deletion
+    } else {
+      res.status(404).send('Document not found'); // Respond with an error message if the document is not found
+    }
+  } catch (error) {
+    console.error('Error deleting document:', error);
+    res.status(500).send('Internal Server Error'); // Respond with an error message
+  }
+});
+
 app.post('/upload', async (req, res) => {
   const details = req.body; // No need for await here
 
   try {
-    // Connect to the database
-    const db = client.db(dbName);
-    const collection = db.collection('enrollment-portal');
     await collection.insertOne(details);
-
     // Find all documents in the collection
     const cursor = collection.find();
     // Convert the cursor to an array of documents
     const data = await cursor.toArray();
     // Display all data from the database
-    console.log(data);
     res.render('template', { data }); // Render the template with the received data
   } catch (error) {
     console.error('Error handling form submission:', error);
